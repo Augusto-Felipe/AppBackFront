1bfcea3b52e3716c9cab96d67946c520527d0bd3
Felipe Augusto <augusto_felipe@icloud.com>
2023-09-17 14:56:21 -0300

    [UPDATE] firebase removed

--- AppBackFront/AppBackFront/SceneDelegate.swift
+++ AppBackFront/AppBackFront/SceneDelegate.swift
@@ -18,9 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-//        let vc: LoginVC = LoginVC()
         let vc: TabBarVC = TabBarVC()
-//        let navVC = UINavigationController(rootViewController: vc)
         window.rootViewController = vc
         window.makeKeyAndVisible()
         self.window = window
